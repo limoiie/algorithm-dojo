757caf1772d1598abfbef7568cab5791f581349b
gengwangli <limo.iie4@gmail.com>
2020-06-12 15:28:21 +0800

    add: Add gitignore

--- leetcode/editor/en/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ligengwang/Projects/clion/algorithm-dojo/leetcode/editor/en
-# Build directory: /Users/ligengwang/Projects/clion/algorithm-dojo/leetcode/editor/en/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTestsInMain "algorithm_dojo")
-set_tests_properties(AllTestsInMain PROPERTIES  _BACKTRACE_TRIPLES "/Users/ligengwang/Projects/clion/algorithm-dojo/leetcode/editor/en/CMakeLists.txt;21;add_test;/Users/ligengwang/Projects/clion/algorithm-dojo/leetcode/editor/en/CMakeLists.txt;0;")
